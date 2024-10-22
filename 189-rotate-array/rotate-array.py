7dfad17a6a44b6c57a746b8677e2d4ec8cfd8f6d
Younsi Hamza <80538927+younsihamza@users.noreply.github.com>
2024-10-22 14:44:12 +0100

    Time: 1011 ms (14.08%) | Memory: 24.6 MB (97.65%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        k = k % len(nums);
         for i in range(0,k):
             nums.insert(0,nums.pop(-1))
         
