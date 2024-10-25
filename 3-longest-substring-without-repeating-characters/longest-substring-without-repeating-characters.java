4ec490af8571618ec550679ccb7a77d81464a257
Younsi Hamza <80538927+younsihamza@users.noreply.github.com>
2024-10-25 14:44:42 +0100

    Time: 669 ms (5.01%) | Memory: 45.6 MB (6.92%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Arrays;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<String> checkSet;
+        int currentIndex = 0;
+        int leftIndex = -1;
+        int maxLen = 0;
+        String hold = "";
+        for (; currentIndex < s.length(); currentIndex++) {
+            hold = s.substring(leftIndex+1,currentIndex+1);
+            checkSet =  new HashSet<String>(Arrays.asList(hold.split("")));
+            System.out.println("hold value : "+ hold + " hold size : " + hold.length() + " set size : " + checkSet.size());
+            if(hold.length() == checkSet.size())
+                maxLen = Math.max(hold.length(),maxLen);
+            while(hold.length() != checkSet.size() && currentIndex >  leftIndex) {
+                leftIndex++;
+                hold = s.substring(leftIndex+1,currentIndex+1);
+                checkSet =  new HashSet<String>(Arrays.asList(hold.split("")));
+            }
+
+        }
+        return maxLen;
+    }
+}
