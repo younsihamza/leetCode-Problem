51db546b8de648e0d3217babda9ec5a1910682d8
Younsi Hamza <80538927+younsihamza@users.noreply.github.com>
2024-11-04 10:12:50 +0100

    Time: 19 ms (7.55%) | Memory: 45.6 MB (5.02%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+class Solution {
+    private boolean isNumber(String token) {
+        try{
+            Integer.parseInt(token);
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+    private int operation(String op, int n2,int n1) {
+        return switch (op) {
+            case "/" -> n1/n2;
+            case "-" -> n1 - n2;
+            case "+" -> n1 + n2;
+            case "*" -> n1 *n2;
+            default -> 0;
+        };
+    }
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> hold = new Stack<>();
+        for(int i  = 0 ; i < tokens.length; i++) {
+            if(isNumber(tokens[i]))
+                hold.push(Integer.parseInt(tokens[i]));
+            else
+                hold.push(operation(tokens[i], hold.pop(), hold.pop()));
+        }
+    return hold.pop();
+    }
+}
