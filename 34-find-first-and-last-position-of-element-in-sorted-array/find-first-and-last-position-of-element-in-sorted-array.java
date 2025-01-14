6185b36b19b1d9e913d8d80eef4e2aa23e402a0b
Younsi Hamza <80538927+younsihamza@users.noreply.github.com>
2025-01-14 16:21:54 +0100

    Time: 1 ms (17.71%) | Memory: 46.3 MB (15.07%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,20 +1,33 @@
 class Solution {
+    private int  binarySearch(int[] nums, int target, int start, int end) {
+        if(start >= end)
+            return -1;
+        int mid =start + (end-end) / 2;
+        if(target == nums[mid])
+            return mid;
+        return (nums[mid]<target ? binarySearch(nums, target, mid+1, end): binarySearch(nums, target, start, mid));
+    }
     public int[] searchRange(int[] nums, int target) {
         int first = -1;
         int second = -1;
-        for(int i = 0; i < nums.length;i++){
-            if(target == nums[i]){
-                first = i;
+        int r = binarySearch(nums,target, 0, nums.length);
+        first = r;
+        second = r;
+        int[] a = {r,r};
+        if(r == -1)
+            return a;
+        for(int i = r; i >= 0;i--){
+            if(target != nums[i]){
                 break;
             }
+            a[0] = i;
         }
-        for(int i = nums.length - 1; i >= 0;i--){
-            if(target == nums[i]){
-                second = i;
+        for(int i = r; i < nums.length;i++){
+            if(target != nums[i]){
                 break;
             }
+            a[1] = i;
         }
-        int[] a = {first,second};
     return a;
     }
 }
